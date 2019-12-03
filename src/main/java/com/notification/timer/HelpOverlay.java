299eacb4d27109c9ebe25bcd4fdb375a89df8d9c
Guillaume Militello <guillaume.militello@gmail.com>
2019-12-02 22:42:25 -0500

    Redo indications with only one screen

--- src/main/java/com/notification/timer/HelpOverlay.java
+++ src/main/java/com/notification/timer/HelpOverlay.java
@@ -14,27 +14,27 @@ class HelpOverlay {
     private final ImageView imageViewForeground;
     private final ImageView imageViewBackground;
 
-    private int currentImageId;
+    private int currentImageClick;
 
     HelpOverlay(MainActivity mainActivity) {
         layout = mainActivity.findViewById(R.id.layoutHelp);
         imageViewBackground = mainActivity.findViewById(R.id.imageViewHelpBackground);
         imageViewForeground = mainActivity.findViewById(R.id.imageViewHelpForeground);
-        TextView textViewInfo = mainActivity.findViewById(R.id.textViewHelpButtonInfo);
-        TextView textViewDone = mainActivity.findViewById(R.id.textViewHelpButtonDone);
+        TextView textViewAbove = mainActivity.findViewById(R.id.textViewHelpAbove);
+        TextView textViewBelow = mainActivity.findViewById(R.id.textViewHelpBelow);
         imageViewForeground.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 next();
             }
         });
-        textViewInfo.setOnClickListener(new View.OnClickListener() {
+        textViewAbove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                next();
+                hide();
             }
         });
-        textViewDone.setOnClickListener(new View.OnClickListener() {
+        textViewBelow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 hide();
@@ -53,48 +53,21 @@ class HelpOverlay {
     void show() {
         Log.d(TAG, "show");
         setVisible(true);
-        currentImageId = 0;
-        showImage(currentImageId);
+        currentImageClick = 0;
+        imageViewForeground.setImageResource(R.drawable.help);
+        imageViewBackground.setImageResource(R.drawable.help_background);
     }
 
     private void next() {
-        Log.d(TAG, "next");
-        showImage(++currentImageId);
+        ++currentImageClick;
+        Log.d(TAG, "next: currentImageClick=" + currentImageClick);
+        if (currentImageClick == 2){
+            hide();
+        }
     }
 
     private void hide() {
         Log.d(TAG, "hide");
         setVisible(false);
-    }
-
-    private void showImage(int imageId) {
-        Log.d(TAG, "showImage: imageId=" + imageId);
-        switch (imageId) {
-            case 0:
-                imageViewForeground.setImageResource(R.drawable.help_0_add_text);
-                imageViewBackground.setImageResource(R.drawable.help_0_add_background);
-                return;
-            case 1:
-                imageViewForeground.setImageResource(R.drawable.help_1_ready_text);
-                imageViewBackground.setImageResource(R.drawable.help_1_ready_background);
-                return;
-            case 2:
-                imageViewForeground.setImageResource(R.drawable.help_2_running_text);
-                imageViewBackground.setImageResource(R.drawable.help_2_running_background);
-                return;
-            case 3:
-                imageViewForeground.setImageResource(R.drawable.help_3_running_text);
-                imageViewBackground.setImageResource(R.drawable.help_2_running_background); // same background as 2
-                return;
-            case 4:
-                imageViewForeground.setImageResource(R.drawable.help_4_ready_text);
-                imageViewBackground.setImageResource(R.drawable.help_4_ready_background);
-                return;
-            case 5:
-                imageViewForeground.setImageResource(R.drawable.help_5_running_text);
-                imageViewBackground.setImageResource(R.drawable.help_5_running_background);
-                return;
-        }
-        hide();
     }
 }
